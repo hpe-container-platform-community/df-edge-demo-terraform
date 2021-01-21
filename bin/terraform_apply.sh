010bdac76e2117852ef88444cfde77034120cc25
Chris Snow <chsnow123@gmail.com>
2021-01-21 21:50:08 +0000

    remove redundant files

--- bin/terraform_apply.sh
+++ bin/terraform_apply.sh
@@ -24,7 +24,6 @@ terraform apply -var-file=etc/bluedata_infra.tfvars \
    -var="client_cidr_block=$(curl -s http://ifconfig.me/ip)/32" "$@"
 
 terraform output -json > generated/output.json 
-#./scripts/post_refresh_or_apply.sh
 
 ./bin/ec2_instance_status.sh
 
--- bin/terraform_refresh.sh
+++ bin/terraform_refresh.sh
@@ -9,10 +9,4 @@ terraform refresh -var-file=etc/bluedata_infra.tfvars \
    -var="client_cidr_block=$(curl -s http://ifconfig.me/ip)/32"
 
 terraform output -json > generated/output.json
-./scripts/post_refresh_or_apply.sh
 
-source ./scripts/variables.sh
-if [[ "$RDP_SERVER_ENABLED" == True && "$RDP_SERVER_OPERATING_SYSTEM" == "LINUX" ]]; then
-   # Display RDP Endpoint and Credentials
-   ./generated/rdp_credentials.sh
-fi
