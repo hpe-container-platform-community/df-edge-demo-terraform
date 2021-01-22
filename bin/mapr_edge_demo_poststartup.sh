880ed608aca74b729dfb7d1865c566a81e527b02
Chris Snow <chsnow123@gmail.com>
2021-01-22 22:18:54 +0000

    fixes

--- bin/mapr_edge_demo_poststartup.sh
+++ bin/mapr_edge_demo_poststartup.sh
@@ -3,27 +3,38 @@
 export HIDE_WARNINGS=1
 
 source "./scripts/variables.sh"
+source "./scripts/functions.sh"
 
+################################################################################
+print_header "Running setup on Edge Cluster - logging in"
+################################################################################
 
 ./bin/ssh_mapr_cluster_2_host_0.sh sudo -u mapr bash <<EOF
-   set -ex
-
     echo mapr | maprlogin password -user mapr -cluster edge1.enterprise.org
     maprlogin authtest -cluster edge1.enterprise.org
 
     echo mapr | maprlogin password -user mapr -cluster dc1.enterprise.org
     maprlogin authtest -cluster dc1.enterprise.org
+EOF
 
-    ######################################################################
+################################################################################
+print_header "Running setup on Edge Cluster - Deleting replicatedStream"
+################################################################################
 
+./bin/ssh_mapr_cluster_2_host_0.sh sudo -u mapr bash <<EOF
     maprcli stream delete \
         -path /mapr/edge1.enterprise.org/apps/pipeline/data/replicatedStream || true
 
     maprcli stream replica remove \
         -path /mapr/dc1.enterprise.org/apps/pipeline/data/replicatedStream \
         -replica /mapr/edge1.enterprise.org/apps/pipeline/data/replicatedStream || true
+EOF
 
-    ######################################################################  
+################################################################################
+print_header "Running setup on Edge Cluster - Setting up Volumes"
+################################################################################
+
+./bin/ssh_mapr_cluster_2_host_0.sh sudo -u mapr bash <<EOF
 
     maprcli volume remove \
         -name files-missionX \
